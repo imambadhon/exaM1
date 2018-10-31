0213d86ddfb7168a665cdf2babeaa838477b045c
Imam Hossain Badhon <43327951+imambadhon@users.noreply.github.com>
2018-10-31 09:25:05 +0600

    Add files via upload

---
+++ day.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<body>
+
+<h2>Day log</h2>
+<form action="/action_page.php">
+  <br>
+  <input type="text" name="ENTER TASK" value="ENTER TASK">
+  <br>
+ <br>
+  <input type="text" name="ENTER TASK TIME" value="ENTER TASK TIME">
+  <br><br>
+ 
+
+<button type="button" onclick="alert('Hello world!')">add</button>
+
+</body>
+</html>
